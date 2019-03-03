30e92452f8982541a83ba1d81bda5845cc9c5b19
aaron.goulden <openheart.three@gmail.com>
2019-03-03 09:31:45 -0600

    Add backbutton on Homepage

--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import TouchableText from '../components/TouchableText';
 
 class Homepage extends Component {
   render() {
+    const { navigation } = this.props;
     return (
       <View style={styles.container}>
         <Text>Homepage</Text>
+        <TouchableText onPress={() => navigation.navigate('Login')}>Back to login</TouchableText>
       </View>
     );
   }
